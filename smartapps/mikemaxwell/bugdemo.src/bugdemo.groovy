dab4249b23148c9fc789249a2197a4b9dbd89e28
Mike Maxwell <madmax98087@yahoo.com>
2016-02-18 19:42:58 -0800

    bugDemo

---
+++ smartapps/mikemaxwell/bugdemo.src/bugdemo.groovy
@@ -0,0 +1,66 @@
+definition(
+    name: "bugDemo",
+    singleInstance: true,
+    namespace: "MikeMaxwell",
+    author: "Mike Maxwell",
+    description: "device selection bug demo",
+    category: "My Apps",
+  	iconUrl: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/Cat-ModeMagic.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/Cat-ModeMagic@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/Cat-ModeMagic@3x.png"
+)
+
+preferences {
+    page(name: "main")
+}
+
+def main(){
+	dynamicPage(name: "main", title: "main", uninstall: false, install: true) {
+        section(){
+        	input(
+				name			: "baseDevice"
+				,title			: "Base Device Type"
+				,multiple		: false
+				,required		: false
+				,type			: "enum"
+                ,options		: ["capability.actuator","device.dimmerSwitch","device.keenHomeSmartVent","device.harmonyActivity"]
+				,submitOnChange	: true
+             )   
+        	 input(
+            	name			: "device"
+                ,title			: "select a device"
+                ,multiple		: false
+                ,required		: false
+                ,type			: settings.baseDevice
+                ,submitOnChange	: true
+            )	
+            if (settings.device){
+            	paragraph(spinOmatic())
+            }
+        }
+    }
+
+}
+
+def spinOmatic(){
+	def data = ""
+		settings.device.properties.each{ p ->
+        	data = data + "${p}\n"
+        }
+	return data
+}
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+	
+}
+
+
